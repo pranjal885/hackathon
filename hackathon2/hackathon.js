3e688d15a3624e98cf573e8f1971e5980241fbe9
pranjal885 <152215501+pranjal885@users.noreply.github.com>
2024-04-14 23:55:14 +0530

    Add files via upload

---
+++ hackathon2/hackathon.js
@@ -0,0 +1,12 @@
+// let map;
+
+// function initMap() {
+//     map = new google.maps.Map(document.getElementById('map'), {
+//         center: {lat: -34.397, lng: 150.644},
+//         zoom: 8
+//     });
+
+//     // Add traffic layer to the map
+//     const trafficLayer = new google.maps.TrafficLayer();
+//     trafficLayer.setMap(map);
+// }
